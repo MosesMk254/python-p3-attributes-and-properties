647494c15b3533a540ec7a4b971c9468dd4ba535
Moses Mutisya <mutisyamoses722@gmail.com>
2024-05-26 12:31:14 +0300

    changes

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,23 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='Fido', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            raise ValueError(
+                "Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+bruno = Dog('Bruno')
+print(bruno.name)
+
+print(Dog)
